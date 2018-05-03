d8bb10f864d3ea51682ead83928dc794674074bc
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2018-05-03 17:24:02 -0400

    updated stuff so rake tasks work;

--- module_1/010_intro_to_active_record/environment.rb
+++ module_1/010_intro_to_active_record/environment.rb
@@ -8,7 +8,4 @@ require_all 'db'
 # setting up the database connection (old way)
 # DB = SQLite3::Database.new("chinook.db")
 
-ActiveRecord::Base.establish_connection({
-	adapter: 'sqlite3',
-	database: 'db/test.db', 
-})
+ActiveRecord::Base.establish_connection :development
